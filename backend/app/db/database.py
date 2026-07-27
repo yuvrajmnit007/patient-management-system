087b10d4f492ed9d0e3a5288670490910ecc74fa
yuvrajchoudhary3768@gmail.com <yuvrajchoudhary3768@gmail.com>
2026-07-27 19:55:04 +0530

    initial setup

---
+++ backend/app/db/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import (  # type: ignore[import-not-found]
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from app.config.settings import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True, future=True)
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
