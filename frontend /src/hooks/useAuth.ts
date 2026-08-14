8d083252d99fdbc2cebc4eb1f9c772c0c3267740
yuvrajchoudhary3768@gmail.com <yuvrajchoudhary3768@gmail.com>
2026-08-14 14:09:02 +0530

    refactor(hooks/useAuth): move hook implementation here for Fast Refresh compatibility

--- frontend /src/hooks/useAuth.ts
+++ frontend /src/hooks/useAuth.ts
@@ -1,3 +1,10 @@
-import { useAuth } from '@/context/AuthContext';
+import { useContext } from 'react';
+import { AuthContext, AuthContextType } from '@/context/AuthContext';
 
-export { useAuth };
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
